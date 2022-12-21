75a3ce4630bc88b427a5e3d726f1eeb8d2b51b82
Abe Abraham <maxabiel12@gmail.com>
2022-12-20 19:42:52 -0500

    i made the person object

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import javax.swing.JFrame;
+
+import java.util.Scanner; 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-------------------\n   Welcome to Go\n-------------------");
+        System.out.println("-------------------\n   Welcome to Go! \n-------------------");
+      
+        // don't remember if there was a scanner  idk :p
+        System.out.println( "What is your first name?: ");
+        int player1_name ;  
+        System.out.println("... So your name is  ");//+ player1_name);
+        
+        int player1_score;
+        	////	
+        enum Colour {
+    	   BLACK, WHITE
+        }
+       // ? new to me 
+        	////
+       boolean player1_winning = true ; 
+        boolean player1_turn = true ;
+        
+        
     }
 }
