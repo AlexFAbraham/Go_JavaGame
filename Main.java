95b10e7dfa0cac981284ae14311dedb40a16ce7c
Abe Abraham <maxabiel12@gmail.com>
2022-12-20 21:07:39 -0500

    README.md

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("-------------------\n   Welcome to Go! \n-------------------");
+      
+      
+    }
+}
